94b2f959217370cdfdea442ee970babdc31dd2f5
BandarCeramica <mhamaddahi.ai@gmail.com>
2026-03-01 02:08:26 +0200

    Add contact form submission alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+const form = document.getElementById("contact-form");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  alert("Thank you for contacting Bandar Ceramica!");
+});
